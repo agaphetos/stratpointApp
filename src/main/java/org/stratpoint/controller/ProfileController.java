64fd538dc4345f91f76c9d77e01618c07aa00fb3
Levin Calado <levincalado@gmail.com>
2017-08-27 01:10:33 +0800

    controller: add CrossOrigin support

--- src/main/java/org/stratpoint/controller/ProfileController.java
+++ src/main/java/org/stratpoint/controller/ProfileController.java
@@ -3,6 +3,7 @@ package org.stratpoint.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +19,14 @@ public class ProfileController {
 	@Autowired
 	private ProfileService profileService;
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
 	public Profile[] getProfiles() {
 		log.info("GET profiles");
 		return profileService.getProfiles();
 	}
-	
+
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/profile/{id}", method = RequestMethod.GET)
 	public Profile getProfile(@PathVariable("id") String id) {
 		log.info("GET profile with id: " + id);
