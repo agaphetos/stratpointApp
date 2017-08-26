64b4adb023736261ba14f2ff0596a47af3e2e3c2
Levin Calado <levincalado@gmail.com>
2017-08-26 19:23:39 +0800

    app: add ComponentScan

--- src/main/java/org/stratpoint/app/StratpointApplication.java
+++ src/main/java/org/stratpoint/app/StratpointApplication.java
@@ -2,10 +2,12 @@ package org.stratpoint.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.stratpoint")
 public class StratpointApplication {
-
+		
 	public static void main(String[] args) {
 		SpringApplication.run(StratpointApplication.class, args);
 	}
