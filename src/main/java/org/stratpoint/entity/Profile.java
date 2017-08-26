65ea16eda0c5428785b969e126e6dacec4f57efc
Levin Calado <levincalado@gmail.com>
2017-08-26 17:33:46 +0800

    entity: add Name and Profile

---
+++ src/main/java/org/stratpoint/entity/Profile.java
@@ -0,0 +1,131 @@
+package org.stratpoint.entity;
+
+import java.util.Date;
+
+public class Profile {
+	private String id;
+	private Boolean active;
+	private Boolean blocked;
+	private String balance;
+	private String picture;
+	private Integer age;
+	private Name name;
+	private String email;
+	private String phone;
+	private String address;
+	private String profile;
+	private Date dateRegistered;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public Boolean getActive() {
+		return active;
+	}
+	
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+	public Boolean getBlocked() {
+		return blocked;
+	}
+	
+	public void setBlocked(Boolean blocked) {
+		this.blocked = blocked;
+	}
+	
+	public String getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	
+	public String getPicture() {
+		return picture;
+	}
+	
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	public Name getName() {
+		return name;
+	}
+	
+	public void setName(Name name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getProfile() {
+		return profile;
+	}
+	
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public Date getDateRegistered() {
+		return dateRegistered;
+	}
+
+	public void setDateRegistered(Date dateRegistered) {
+		this.dateRegistered = dateRegistered;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id + 
+				", active=" + active +
+				", blocked=" + blocked + 
+				", balance='" + balance + '\'' +
+				", age=" + age + 
+				", name=" + name + 			
+				", email='" + email + '\'' +		
+				", phone='" + phone + '\'' +		
+				", address='" + address + '\'' +		
+				", profile='" + profile + '\'' +
+				", date_registered=" + dateRegistered + 
+				'}';
+	}
+}
